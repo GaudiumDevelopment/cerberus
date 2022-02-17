62adc350456a8d2f76c01625d57039f5c9bffc35
omegabiebel <superbiebel@gmail.com>
2022-02-17 20:40:10 +0100

    fix(CORE): actually added constructor params for final class variables
    Signed-off-by: omegabiebel <superbiebel@gmail.com>

--- src/main/java/me/superbiebel/objects/ObjectPermission.java
+++ src/main/java/me/superbiebel/objects/ObjectPermission.java
@@ -1,9 +1,19 @@
 package me.superbiebel.objects;
 
-import me.superbiebel.objects.logicgates.LogicGate;
-
 public class ObjectPermission implements UserSet{
     public final String permissionName;
     public final boolean userSetRewrite; // if this permission actually calculates it's userset.
     public final UserSet contents;
+    
+    public ObjectPermission(String permissionName) {
+        this.permissionName = permissionName;
+        this.userSetRewrite = false;
+        this.contents = null;
+    }
+    
+    public ObjectPermission(UserSet contents) {
+        this.contents = contents;
+        this.userSetRewrite = true;
+        this.permissionName = null;
+    }
 }
